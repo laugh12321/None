0d2ceef4f4579b018204cb1e81bdb21544da0201
Laugh <laugh12321@vip.qq.com>
2022-04-06 09:17:58 +0800

    Add data

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -1,113 +1,44 @@
-import logging
-from os import listdir
-from os.path import splitext
 from pathlib import Path
-
-import numpy as np
-import torch
-from PIL import Image
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader, random_split
+from typing import Optional
 
 # ⚡ PyTorch Lightning
 import pytorch_lightning as pl
 
-class BasicDataset(Dataset):
-    def __init__(self, images_dir: str, masks_dir: str, scale: float = 1.0, mask_suffix: str = ''):
-        self.images_dir = Path(images_dir)
-        self.masks_dir = Path(masks_dir)
-        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
-        self.scale = scale
-        self.mask_suffix = mask_suffix
-
-        self.ids = [splitext(file)[0] for file in listdir(images_dir) if not file.startswith('.')]
-        if not self.ids:
-            raise RuntimeError(f'No input file found in {images_dir}, make sure you put your images there')
-        logging.info(f'Creating dataset with {len(self.ids)} examples')
-
-    def __len__(self):
-        return len(self.ids)
-
-    @classmethod
-    def preprocess(cls, pil_img, scale, is_mask):
-        w, h = pil_img.size
-        newW, newH = int(scale * w), int(scale * h)
-        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
-        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
-        img_ndarray = np.asarray(pil_img)
-
-        if img_ndarray.ndim == 2 and not is_mask:
-            img_ndarray = img_ndarray[np.newaxis, ...]
-        elif not is_mask:
-            img_ndarray = img_ndarray.transpose((2, 0, 1))
-
-        if not is_mask:
-            img_ndarray = img_ndarray / 255
-
-        return img_ndarray
-
-    @classmethod
-    def load(cls, filename):
-        ext = splitext(filename)[1]
-        if ext in ['.npz', '.npy']:
-            return Image.fromarray(np.load(filename))
-        elif ext in ['.pt', '.pth']:
-            return Image.fromarray(torch.load(filename).numpy())
-        else:
-            return Image.open(filename)
-
-    def __getitem__(self, idx):
-        name = self.ids[idx]
-        mask_file = list(self.masks_dir.glob(name + self.mask_suffix + '.*'))
-        img_file = list(self.images_dir.glob(name + '.*'))
-
-        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
-        assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
-        mask = self.load(mask_file[0])
-        img = self.load(img_file[0])
-
-        assert img.size == mask.size, \
-            'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
-
-        img = self.preprocess(img, self.scale, is_mask=False)
-        mask = self.preprocess(mask, self.scale, is_mask=True)
-
-        return {
-            'image': torch.as_tensor(img.copy()).float().contiguous(),
-            'mask': torch.as_tensor(mask.copy()).long().contiguous()
-        }
-
-
-class CarvanaDataset(BasicDataset):
-    def __init__(self, images_dir, masks_dir, scale=1):
-        super().__init__(images_dir, masks_dir, scale, mask_suffix='_mask')
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 
 class Dataset(pl.LightningDataModule):
-    def __init__(self, images_dir, masks_dir, val, scale=1, batch_size=1):
+    def __init__(self, data_dir, scale=0.5, batch_size=1):
         super().__init__()
-        self.images_dir = images_dir
-        self.masks_dir = masks_dir
-        self.val = val
-        self.scale = scale
-        self.batch_size = batch_size
-    
-    def prepare_data(self):
-        # 1. Create dataset
-        try:
-            self.dataset = CarvanaDataset(self.images_dir, self.masks_dir, self.scale)
-        except (AssertionError, RuntimeError):
-            self.dataset = BasicDataset(self.images_dir, self.masks_dir, self.scale)
-    
-    def setup(self, stage=None):
-        # we set up only relevant datasets when stage is specified
 
-        # 2. Split into train / validation partitions
+        self.val_dir = Path(data_dir, 'val2017')
+        self.train_dir = Path(data_dir, 'train2017')
+        self.val_annFile = Path(data_dir, 'annotations/instances_val2017.json')
+        self.train_annFile = Path(data_dir, 'annotations/instances_train2017.json')
+
+        h, w = 380, 676
+        H, W = int(scale * h), int(scale * w)
+        self.transform = transforms.Compose([transforms.Resize([H, W]),
+                                            transforms.ToTensor(), 
+                                            transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                 [0.229, 0.224, 0.225])])
+
+        self.loader_args = dict(batch_size=batch_size, num_workers=4, pin_memory=True)
+    
+    # def prepare_data(self):
+    #     # 1. Create dataset
+    #     try:
+    #         self.dataset = CarvanaDataset(self.images_dir, self.masks_dir, self.scale)
+    #     except (AssertionError, RuntimeError):
+    #         self.dataset = BasicDataset(self.images_dir, self.masks_dir, self.scale)
+    
+    def setup(self, stage: Optional[str] = None):
+        # Assign train/val datasets for use in dataloaders
         if stage == 'fit' or stage is None:
-            n_val = int(len(self.dataset) * self.val)
-            n_train = len(self.dataset) - n_val
-            self.train_set, self.val_set = random_split(self.dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0))
-            self.loader_args = dict(batch_size=self.batch_size, num_workers=4, pin_memory=True)
+            self.val_set = datasets.CocoDetection(self.val_dir, self.val_annFile, transform=self.transform) 
+            self.train_set = datasets.CocoDetection(self.train_dir, self.train_annFile, transform=self.transform)
+            
         # if stage == 'test' or stage is None:
         #     self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
     
