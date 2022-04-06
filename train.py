2f3e3236659cd7c05bed1c65cfa375d0f70724c2
Laugh <laugh12321@vip.qq.com>
2022-04-06 21:41:41 +0800

    Update trainer

--- train.py
+++ train.py
@@ -1,86 +1,212 @@
+import sys
+import random
+import logging
 import argparse
+import numpy as np
+from tqdm import tqdm
 from pathlib import Path
 
-# 🏋️‍♀️ Weights & Biases
 import wandb
-# ⚡ PyTorch Lightning
-import pytorch_lightning as pl
-# ⚡ 🤝 🏋️‍♀️
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import transforms
+import torch.backends.cudnn as cudnn
+from torch.utils.data import DataLoader, random_split
 
-from utils.data_loading import CarvanaDataset
-from networks.model import TransUnet, ImagePredictionLogger
+from networks.vit_seg_modeling import VisionTransformer as ViT_seg
 from networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
+from utils.data_loading import BasicDataset
+from utils.dice_score import dice_loss
+from evaluate import evaluate
 
 dir_root = Path('./data/')
 dir_checkpoint = Path('./checkpoints/')
 
 
+def train_net(net,
+              device,
+              epochs: int = 5,
+              batch_size: int = 1,
+              learning_rate: float = 1e-5,
+              val_percent: float = 0.1,
+              save_checkpoint: bool = True,
+              img_size: int = 224,
+              amp: bool = False):
+    # 1. Defining transforms to be applied on the data
+    transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+    target_transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor()
+    ])
+
+    # 2. Create dataset
+    try:
+        dataset = BasicDataset(root=dir_root,  mask_suffix='_mask', 
+                               transform=transform, target_transform=target_transform)
+    except (AssertionError, RuntimeError):
+        dataset = BasicDataset(root=dir_root, transform=transform, 
+                               target_transform=target_transform)
+
+    # 2. Split into train / validation partitions
+    n_val = int(len(dataset) * val_percent)
+    n_train = len(dataset) - n_val
+    train_set, val_set = random_split(dataset, [n_train, n_val], generator=torch.Generator())
+
+    # 3. Create data loaders
+    loader_args = dict(batch_size= batch_size, num_workers=2, pin_memory=True)
+    train_loader = DataLoader(train_set, shuffle=True, **loader_args)
+    val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
+
+    # (Initialize logging)
+    experiment = wandb.init(project='TransUNet', resume='allow', anonymous='must')
+    experiment.config.update(dict(epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
+                                  val_percent=val_percent, save_checkpoint=save_checkpoint, img_size=img_size,
+                                  amp=amp))
+
+    logging.info(f'''Starting training:
+        Epochs:          {epochs}
+        Batch size:      {batch_size}
+        Learning rate:   {learning_rate}
+        Training size:   {n_train}
+        Validation size: {n_val}
+        Checkpoints:     {save_checkpoint}
+        Device:          {device.type}
+        Image size:      {img_size}
+        Mixed Precision: {amp}
+    ''')
+
+    # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
+    optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.9)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)  # goal: maximize Dice score
+    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
+    criterion = nn.CrossEntropyLoss()
+    global_step = 0
+
+    # 5. Begin training
+    for epoch in range(epochs):
+        net.train()
+        epoch_loss = 0
+        with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                images, true_masks = batch
+
+                images = images.to(device=device, dtype=torch.float32)
+                true_masks = true_masks.to(device=device, dtype=torch.long)
+
+                with torch.cuda.amp.autocast(enabled=amp):
+                    masks_pred = net(images)
+                    loss = criterion(masks_pred, true_masks) \
+                           + dice_loss(F.softmax(masks_pred, dim=1).float(),
+                                       F.one_hot(true_masks, net.n_classes).permute(0, 3, 1, 2).float(),
+                                       multiclass=True)
+
+                optimizer.zero_grad(set_to_none=True)
+                grad_scaler.scale(loss).backward()
+                grad_scaler.step(optimizer)
+                grad_scaler.update()
+    
+                pbar.update(images.shape[0])
+                global_step += 1
+                epoch_loss += loss.item()
+                experiment.log({
+                    'train loss': loss.item(),
+                    'step': global_step,
+                    'epoch': epoch
+                })
+                pbar.set_postfix(**{'loss (batch)': loss.item()})
+
+                # Evaluation round
+                division_step = (n_train // (10 * batch_size))
+                if division_step > 0:
+                    if global_step % division_step == 0:
+                        val_score = evaluate(net, val_loader, device)
+                        scheduler.step(val_score)
+
+                        logging.info('Validation Dice score: {}'.format(val_score))
+                        experiment.log({
+                            'learning rate': optimizer.param_groups[0]['lr'],
+                            'validation Dice': val_score,
+                            'images': wandb.Image(images[0].cpu()),
+                            'masks': {
+                                'true': wandb.Image(true_masks[0].float().cpu()),
+                                'pred': wandb.Image(torch.softmax(masks_pred, dim=1).argmax(dim=1)[0].float().cpu()),
+                            },
+                            'step': global_step,
+                            'epoch': epoch
+                        })
+
+        if save_checkpoint:
+            Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
+            torch.save(net.state_dict(), str(dir_checkpoint / 'checkpoint_epoch{}.pth'.format(epoch + 1)))
+            logging.info(f'Checkpoint {epoch + 1} saved!')
+
+
 def get_args():
-    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
-    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--batch_size', '-b', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--lr', '-l', metavar='LR', type=float, default=0.01, help='Learning rate')
-    parser.add_argument('--num_classes', '-n', type=int, default=2, help='output channel of network')
-    parser.add_argument('--val', '-v', type=float, default=10.0, help='Percent of the data that is used as validation (0-100)')
+    parser = argparse.ArgumentParser(description='Train the TransUNet on images and target masks')
+    parser.add_argument('--epochs', '-e',  type=int, default=5, help='Number of epochs')
+    parser.add_argument('--batch_size', '-b', dest='batch_size', type=int, default=1, help='Batch size')
+    parser.add_argument('--learning_rate', '-l', dest='lr', type=float, default=1e-5, help='Learning rate')
+    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0, help='Percent of the data that is used as validation (0-100)')
+    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
+    parser.add_argument('--num_classes', type=int, default=2, help='output channel of network')
+    parser.add_argument('--img_size', type=int, default=224, help='input patch size of network input')
+    parser.add_argument('--seed', type=int, default=1234, help='random seed')
+    parser.add_argument('--n_skip', type=int, default=3, help='using number of skip-connect, default is num')
     parser.add_argument('--vit_name', type=str, default='R50-ViT-B_16', help='select one vit model')
     parser.add_argument('--vit_patches_size', type=int, default=16, help='vit_patches_size, default is 16')
-    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
-
-    return parser.parse_args()
+    parser.add_argument('--deterministic', action='store_true', default=False, help='whether use deterministic training')
+    return parser.parse_args()     
 
 
 if __name__ == '__main__':
-    # init
     args = get_args()
-    wandb_logger = WandbLogger(project="TransUnet")
-    pl.seed_everything(hash("setting random seeds") % 2**32 - 1)
 
-    # setup data
-    data = CarvanaDataset(root_dir=dir_root, batch_size=args.batch_size, 
-                          val_percent=args.val, num_workers=8)
-    data.prepare_data()
-    data.setup()
+    if not args.deterministic:
+        cudnn.benchmark = True
+        cudnn.deterministic = False
+    else:
+        cudnn.benchmark = False
+        cudnn.deterministic = True
 
-    # grab samples to log predictions on
-    samples = next(iter(data.val_dataloader()))
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
 
-    # setup model
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')
+
     config_vit = CONFIGS_ViT_seg[args.vit_name]
     config_vit.n_classes = args.num_classes
     config_vit.n_skip = args.n_skip
     if args.vit_name.find('R50') != -1:
         config_vit.patches.grid = (int(args.img_size / args.vit_patches_size), int(args.img_size / args.vit_patches_size))
-    model = TransUnet(config_vit, img_size=args.img_size, lr=args.lr, amp=args.amp)
+    net = ViT_seg(config_vit, img_size=args.img_size, num_classes=config_vit.n_classes).to(device=device)
+    net.load_from(weights=np.load(config_vit.pretrained_path))
 
-    # checkpoints
-    checkpoint = ModelCheckpoint(
-        dirpath = dir_checkpoint,
-        filename ='TransUnet-{epoch:02d}-{val_loss:.2f}',
-        monitor ='val_loss',
-        save_top_k = 2,
-        verbose = True,
-    )
+    logging.info(f'Network:\n'
+                 f'\t{args.num_classes} output classes\n'
+                 f'\t{args.img_size} input patch size\n'
+                 f'\t{args.vit_name} vit model\n'
+                 f'\t{"Use deterministic" if args.deterministic else "Use benchmark"} training')
 
-    # earlystopping
-    earlystopping = EarlyStopping(
-        monitor = 'val_loss',
-        mode = 'min',
-        patience = 5, 
-        verbose = True,
-    )
-
-    trainer = pl.Trainer(
-        logger=wandb_logger,    # W&B integration
-        # log_every_n_steps=50,   # set the logging frequency
-        gpus=-1,                # use all GPUs
-        max_epochs=args.epochs,           # number of epochs
-        # deterministic=True,     # keep it deterministic
-        callbacks=[checkpoint, earlystopping, ImagePredictionLogger(samples)] # see Callbacks section
-        )
-    
-    # fit the model
-    trainer.fit(model, data)
-
-    wandb.finish()
+    try:
+        train_net(net=net,
+                  epochs=args.epochs,
+                  batch_size=args.batch_size,
+                  learning_rate=args.lr,
+                  device=device,
+                  img_size=args.img_size,
+                  val_percent=args.val / 100,
+                  amp=args.amp)
+    except KeyboardInterrupt:
+        torch.save(net.state_dict(), 'INTERRUPTED.pth')
+        logging.info('Saved interrupt')
+        sys.exit(0)
