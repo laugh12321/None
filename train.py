a924e255229cbafb8980afc628840189f645b1ec
Laugh <laugh12321@vip.qq.com>
2022-04-06 16:18:46 +0800

    Update args

--- train.py
+++ train.py
@@ -20,12 +20,12 @@ dir_checkpoint = Path('./checkpoints/')
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=0.01,
-                        help='Learning rate', dest='lr')
-    parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
-    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
-                        help='Percent of the data that is used as validation (0-100)')
+    parser.add_argument('--batch_size', '-b', metavar='B', type=int, default=1, help='Batch size')
+    parser.add_argument('--lr', '-l', metavar='LR', type=float, default=0.01, help='Learning rate')
+    parser.add_argument('--num_classes', '-n', type=int, default=2, help='output channel of network')
+    parser.add_argument('--val', '-v', type=float, default=10.0, help='Percent of the data that is used as validation (0-100)')
+    parser.add_argument('--vit_name', type=str, default='R50-ViT-B_16', help='select one vit model')
+    parser.add_argument('--vit_patches_size', type=int, default=16, help='vit_patches_size, default is 16')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
 
     return parser.parse_args()
